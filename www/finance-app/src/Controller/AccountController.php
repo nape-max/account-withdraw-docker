1b950d524ffee924ba2e20535c0584e4675e7b1c
Max Napedenin <napemax@yandex.ru>
2022-04-12 15:51:44 +0300

    Fixed types, fixed access to private field.

--- www/finance-app/src/Controller/AccountController.php
+++ www/finance-app/src/Controller/AccountController.php
@@ -40,9 +40,9 @@ class AccountController
     {
         if (isset($_COOKIE['access_token'])) {
             $accountService = new AccountService(new AccountRepository(new MysqlConnection()), new AccountMapper());
-            $user = $accountService->getAccountByAccessToken($_COOKIE['access_token']);
+            $account = $accountService->getAccountByAccessToken($_COOKIE['access_token']);
 
-            if ($accountService->setAccessTokenByUsername(null, $user->username)) {
+            if ($accountService->setAccessTokenByUsername(null, $account->getUsername())) {
                 setcookie('access_token', false);
             }
         }
@@ -93,10 +93,10 @@ class AccountController
         $accountService = new AccountService(new AccountRepository(new MysqlConnection()), new AccountMapper());
         $view = new View();
 
-        $user = $accountService->getAccountByAccessToken($_COOKIE['access_token']);
+        $account = $accountService->getAccountByAccessToken($_COOKIE['access_token']);
 
-        $data['username'] = $user->getUsername();
-        $data['balance'] = (string) $user->getBalance();
+        $data['username'] = $account->getUsername();
+        $data['balance'] = (string) $account->getBalance();
 
         $view->generate('Account', 'AccountView', $data);
     }
@@ -120,11 +120,11 @@ class AccountController
 
         $accountService = new AccountService(new AccountRepository(new MysqlConnection()), new AccountMapper());
 
-        $userByAccessToken = $accountService->getAccountByAccessToken($accessTokenFromCookie);
-        $userByUsername = $accountService->getAccountByUsername($usernameFromRequest);
+        $accountByAccessToken = $accountService->getAccountByAccessToken($accessTokenFromCookie);
+        $accountByUsername = $accountService->getAccountByUsername($usernameFromRequest);
 
-        if (null !== $userByUsername) {
-            if ($accountService->withdrawFromBalance($amountFromRequest, $passwordFromRequest, $userByUsername, $userByAccessToken)) {
+        if (null !== $accountByUsername) {
+            if ($accountService->withdrawFromBalance($amountFromRequest, $passwordFromRequest, $accountByUsername, $accountByAccessToken)) {
                 header("Location: /account");
                 sleep(1); // Чтобы страница с успехом отображалась позже и на обоих страницах не было ошибки "Не удалось списать средства из-за куки установленной на другой странице", не влияет на логику - просто недоработка механизма вывода ошибок на фронтенд и общения между View.
                 setcookie('is_withdraw_failed', false, 0, "/account", "finance-app", false, true);
